25fe74ef192674fbacde6145935c30cf2c0ccc6d
Maciej <maciejszepler@gmail.com>
2018-04-02 13:44:55 +0200

    chore(): changed user display

--- app/index.js
+++ app/index.js
@@ -41,31 +41,54 @@ window.onload = () => {
             const userName = usersList[i].name;
             companiesList[userIndex].users.push(userName);
           }
-          console.log('#Test Company' + 124 + ':', companiesList[124]);
         })
         .then(() => {
-          let tableText = '<table class="table">\n' +
-                '  <thead>\n' +
+          let tableText = '<table class="table table-hover">\n' +
+                '  <thead class="thead-dark">\n' +
                 '    <tr>\n' +
                 '      <th scope="col">Index</th>\n' +
                 '      <th scope="col">Company Name</th>\n' +
                 '      <th scope="col">URI</th>\n' +
-                '      <th scope="col">Users</th>\n' +
                 '    </tr>\n' +
                 '  </thead>\n' +
                 '  <tbody>';
 
           for(let i = 0; i < companiesList.length; i++) {
-            tableText = tableText +
+            tableText +=
                   '<tr>\n' +
-                  '      <th scope="row">'+ i +'</th>\n' +
+                  '      <td>'+ i +'</td>\n' +
                   '      <td>'+ companiesList[i].name +'</td>\n' +
                   '      <td>'+ companiesList[i].uri +'</td>\n' +
-                  '      <td>'+ companiesList[i].users +'</td>\n' +
                   '</tr>';
+
+            if(companiesList[i].users.length > 0) {
+              tableText +=
+                  '<tr>\n' +
+                  '      <td colspan="3" >\n' +
+                  '         <div >\n' +
+                  '             <table class="table table-bordered">\n' +
+                  '                 <thead class="bg-success">\n' +
+                  '                     <th>User</th>\n' +
+                  '                 </thead>\n' +
+                  '                 <tbody>\n';
+
+              for(let j = 0; j < companiesList[i].users.length; j++) {
+                tableText +=
+                    '<tr>\n' +
+                        '<td>'+ companiesList[i].users[j] +'</td>\n' +
+                    '</tr>\n';
+              }
+
+              tableText +=
+                    '</tbody>\n' +
+                  '</table>\n' +
+                  '</div>\n' +
+                  '</td>\n' +
+                  '</tr>\n';
+            }
           }
 
-          tableText = tableText + '</tbody>\n' + '</table>';
+          tableText += '</tbody>\n' + '</table>';
           document.getElementById('table').innerHTML = tableText;
 
         });
