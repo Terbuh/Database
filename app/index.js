6d930d62daf1f9f68610e4dc05822d9dda0fbcc0
Maciej <maciejszepler@gmail.com>
2018-04-03 16:41:35 +0200

    chore(): code cleanup and different onclick event approach

--- app/index.js
+++ app/index.js
@@ -1,18 +1,36 @@
 import './styles/styles.scss';
-
+/*exported toggleUsers */
 const port = 3000;
 const usersURL = 'http://localhost:' + port + '/users';
 const companiesURL = 'http://localhost:' + port + '/companies';
-
 let usersList = [];
 let companiesList = [];
+
+const tableStart =
+    '<table id="companiesTable" class="table table-hover">\n' +
+    '  <thead class="thead-dark">\n' +
+    '    <tr>\n' +
+    '      <th scope="col">Index</th>\n' +
+    '      <th scope="col">Company Name</th>\n' +
+    '      <th scope="col">URI</th>\n' +
+    '    </tr>\n' +
+    '  </thead>\n' +
+    '  <tbody>';
+
+const tableEnd =
+    '  </tbody>\n' +
+    '</table>';
+
+const hideRow = 'style="display: none"';
 
 window.onload = () => {
 
   fetch(usersURL)
+
     .then(response => response.json()) // zmiana danych do jsona
+
     .then(userData => {
-      console.log('#Full user data: ', userData);
+
       for (let i = 0; i < userData.length; i++) {
         usersList.push({
           name: userData[i].name,
@@ -23,9 +41,11 @@ window.onload = () => {
       }
 
       fetch(companiesURL)
+
         .then(response => response.json())
+
         .then(companiesData => {
-          console.log('#Full companies data: ', companiesData);
+
           for (let i = 0; i < companiesData.length; i++) {
             companiesList.push({
               name: companiesData[i].name,
@@ -35,61 +55,44 @@ window.onload = () => {
           }
 
         })
-        .then(() => {
+
+        .then( () => {
+
           for(let i = 0; i < usersList.length; i++) {
             const userIndex = usersList[i].companyNumber;
             const userName = usersList[i].name;
             companiesList[userIndex].users.push(userName);
           }
         })
+
         .then(() => {
-          let tableText = '<table class="table table-hover">\n' +
-                '  <thead class="thead-dark">\n' +
-                '    <tr>\n' +
-                '      <th scope="col">Index</th>\n' +
-                '      <th scope="col">Company Name</th>\n' +
-                '      <th scope="col">URI</th>\n' +
-                '    </tr>\n' +
-                '  </thead>\n' +
-                '  <tbody>';
+
+          let tableText = tableStart;
 
           for(let i = 0; i < companiesList.length; i++) {
             tableText +=
-                  '<tr>\n' +
+                  '<tr >\n' +
                   '      <td>'+ i +'</td>\n' +
                   '      <td>'+ companiesList[i].name +'</td>\n' +
                   '      <td>'+ companiesList[i].uri +'</td>\n' +
                   '</tr>';
 
             if(companiesList[i].users.length > 0) {
-              tableText +=
-                  '<tr>\n' +
-                  '      <td colspan="3" >\n' +
-                  '         <div >\n' +
-                  '             <table class="table table-bordered">\n' +
-                  '                 <thead class="bg-success">\n' +
-                  '                     <th>User</th>\n' +
-                  '                 </thead>\n' +
-                  '                 <tbody>\n';
 
               for(let j = 0; j < companiesList[i].users.length; j++) {
                 tableText +=
-                    '<tr>\n' +
-                        '<td>'+ companiesList[i].users[j] +'</td>\n' +
+                    '<tr class="table-active" ' + hideRow + '>\n' +
+                    '    <td>' + i + '.' + (j+1) +  '</td>\n' +
+                    '    <td colspan="2">'+ companiesList[i].users[j] +'</td>\n' +
                     '</tr>\n';
               }
 
-              tableText +=
-                    '</tbody>\n' +
-                  '</table>\n' +
-                  '</div>\n' +
-                  '</td>\n' +
-                  '</tr>\n';
             }
           }
 
-          tableText += '</tbody>\n' + '</table>';
+          tableText += tableEnd;
           document.getElementById('table').innerHTML = tableText;
+          addRowHandlers();
 
         });
 
@@ -97,6 +100,23 @@ window.onload = () => {
 
 
 
-
-
 };
+
+function addRowHandlers() {
+
+  let table = document.getElementById('companiesTable');
+  let rows = table.getElementsByTagName('tr');
+  for (let i = 0; i < rows.length; i++) {
+    let currentRow = table.rows[i];
+    let createClickHandler = row => {
+      return () => {
+        let cell = row.getElementsByTagName('td')[0];
+        let id = cell.innerHTML;
+        alert('id:' + id);
+      };
+    };
+
+    currentRow.onclick = createClickHandler(currentRow);
+  }
+
+}
