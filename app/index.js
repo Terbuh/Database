f7165c6688585d9191187f6415fac168061c0945
Terbuh <hubertt.popowicz@gmail.com>
2021-09-28 19:28:26 +0200

    feat(3rd-function): add 3rd function

--- app/index.js
+++ app/index.js
@@ -6,34 +6,61 @@
 // };
 const container = document.querySelector('.users');
 const container2 = document.querySelector('.companies');
+const container3 = document.querySelector('.companiesanditsusers');
 
-const renderUsers = async () => {
-    let uri = 'http://localhost:3000/users';
+// const renderUsers = async () => {
+//     let uri = 'http://localhost:3000/users';
 
-    const res = await fetch(uri);
-    const users = await res.json();
+//     const res = await fetch(uri);
+//     const users = await res.json();
 
-    let template = '';
-    users.forEach(user => {
-        template += `
-        <div class="user">
-        <h2>Name: ${user.name}</h2>
-        <p>Uri: ${user.uri}</p>
-        <p>email: ${user.email}</p>
-        <p>${user.uris}</p>
-        <a href="/details.html"></a>
-        </div>
-        `
-    })
-container.innerHTML = template;
+//     let template = '';
+//     users.forEach(user => {
+//         template += `
+//         <div class="user">
+//         <h2>Name: ${user.name}</h2>
+//         <p>Uri: ${user.uri}</p>
+//         <p>email: ${user.email}</p>
+//         <p>${user.uris.company}</p>
+//         <a href="/details.html"></a>
+//         </div>
+//         `
+//     })
+// container.innerHTML = template;
 
-}
+// }
 
-const renderCompanies = async () => {
-    let uri = 'http://localhost:3000/companies';
+// const renderCompanies = async () => {
+//     let uri = 'http://localhost:3000/companies';
 
-    const res = await fetch(uri);
-    const companies = await res.json();
+//     const res = await fetch(uri);
+//     const companies = await res.json();
+
+//     let template = '';
+//     companies.forEach(company => {
+//         template += `
+//         <div class="company">
+//         <h2>Name: ${company.name}</h2>
+//         <p>Uri: ${company.uri}</p>
+//         <a href="/details.html"></a>
+//         </div>
+//         `
+//     })
+// container2.innerHTML = template;
+
+// }
+
+
+
+const renderCompaniesAndItsUsers = async () => {
+    let uriCompanies = 'http://localhost:3000/companies';
+    let uriUsers = 'http://localhost:3000/users';
+
+    const resCompanies = await fetch(uriCompanies);
+    const companies = await resCompanies.json();
+
+    const resUsers = await fetch(uriUsers);
+    const users = await resUsers.json();
 
     let template = '';
     companies.forEach(company => {
@@ -44,11 +71,24 @@ const renderCompanies = async () => {
         <a href="/details.html"></a>
         </div>
         `
+        users.forEach(user => {
+            if ( company.uri === user.uris.company ) {
+                template += `
+                <div class="user">
+                <h2>Name: ${user.name}</h2>
+                <p>Uri: ${user.uri}</p>
+                <p>email: ${user.email}</p>
+                <a href="/details.html"></a>
+                </div>
+                `
+            }
+        })
     })
-container2.innerHTML = template;
+    container3.innerHTML = template;
 
 }
 
-window.addEventListener('DOMContentLoaded', () => renderUsers());
-window.addEventListener('DOMContentLoaded', () => renderCompanies());
+window.addEventListener('DOMContentLoaded', () => renderCompaniesAndItsUsers());
+// window.addEventListener('DOMContentLoaded', () => renderUsers());
+// window.addEventListener('DOMContentLoaded', () => renderCompanies());
 
